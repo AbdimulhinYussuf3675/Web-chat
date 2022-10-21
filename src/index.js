736ee380d23888ee279a1f5556504410c1f0213f
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2022-10-21 17:59:03 +0300

    Updated variable

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { messagesReducer } from "./data/messagesSlice";
-import { usersReducer } from "./data/usersSlice";
-import {
-  createStateSyncMiddleware,
-  initMessageListener,
-} from "redux-state-sync";
-import { persistReducer } from "redux-persist";
-import { excludedActions, persistConfig } from "./consts";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import data from "./data";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-const rootReducer = combineReducers({
-  users: usersReducer,
-  messages: messagesReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat(
-      createStateSyncMiddleware({
-        blacklist: excludedActions,
-      })
-    ),
-});
-
-initMessageListener(store);
-
-export default store;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={data}>
+      <PersistGate loading={null} persistor={persistStore(data)}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
+reportWebVitals();
